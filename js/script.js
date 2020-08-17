7d1befadb0e4b60bcb015183dd004c779a8a5370
Aleksey <ancientt1k1@gmail.com>
2020-08-17 18:23:56 +0300

    adding Babel+core.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ import cards from './modules/cards';
 import calc from './modules/calc'; 
 import forms from './modules/forms';
 import slider from './modules/slider';
-import {openModal} from './modules/modal'
+import {openModal} from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', () =>{  
     const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
